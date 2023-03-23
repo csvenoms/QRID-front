3eff4bd2585a136f429f66f35e7a80373fa6a52f
Fasika <damtewfasika4@gmail.com>
2023-03-23 16:53:52 +0300

    frontend repo

---
+++ lib/view/splash.view.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_application_1/utils/global.colors.dart';
+
+class SplashView extends StatelessWidget {
+  const SplashView({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+    backgroundColor: GlobalColors.mainColor,
+    );
+  }
+}
