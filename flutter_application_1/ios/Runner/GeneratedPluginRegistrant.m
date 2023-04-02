11953884cefedd83ca7750038f1c7c180e543fbd
Etsubdink-Zebre <etsubdinkzebre09@gmail.com>
2023-04-02 19:38:15 +0300

    etsub

---
+++ flutter_application_1/ios/Runner/GeneratedPluginRegistrant.m
@@ -0,0 +1,21 @@
+//
+//  Generated file. Do not edit.
+//
+
+// clang-format off
+
+#import "GeneratedPluginRegistrant.h"
+
+#if __has_include(<keyboard_utils/SwiftKeyboardUtilsPlugin.h>)
+#import <keyboard_utils/SwiftKeyboardUtilsPlugin.h>
+#else
+@import keyboard_utils;
+#endif
+
+@implementation GeneratedPluginRegistrant
+
++ (void)registerWithRegistry:(NSObject<FlutterPluginRegistry>*)registry {
+  [SwiftKeyboardUtilsPlugin registerWithRegistrar:[registry registrarForPlugin:@"SwiftKeyboardUtilsPlugin"]];
+}
+
+@end
